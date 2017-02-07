b9576d498703adc2f9237e23c210a25c424b9fbc
Codesign <cs@csdev.io>
2017-02-07 00:10:05 +0000

    Allow flasked config to accept map_data directly

--- lib/flasked/config.ex
+++ lib/flasked/config.ex
@@ -2,16 +2,24 @@ defmodule Flasked.Config do
   @moduledoc false
 
   def check do
-    unless keys_present? do
-      raise(ArgumentError, message: "Missing :otp_app and/or :map_file in :flasked configuration")
+    unless otp_app_key_present?, do: raise(ArgumentError, message: ":otp_app is missing in :flasked configuration")
+    unless map_key_present? do
+      raise(ArgumentError, message: "Either :map_file or :map_data is missing in :flasked configuration")
     end
   end
 
-  defp keys_present? do
-    flasked_env = Application.get_all_env(:flasked)
-    Dict.has_key?(flasked_env, :otp_app) && Dict.has_key?(flasked_env, :map_file)
+  defp otp_app_key_present? do
+    Dict.has_key?(get_env, :otp_app)
   end
+
+  defp map_key_present? do
+    env = get_env
+    Dict.has_key?(env, :map_file) || Dict.has_key?(env, :map_data)
+  end
+
+  defp get_env, do: Application.get_all_env(:flasked)
 
   def otp_app, do: Application.get_env(:flasked, :otp_app)
   def map_file, do: Application.get_env(:flasked, :map_file)
+  def map_data, do: Application.get_env(:flasked, :map_data)
 end
